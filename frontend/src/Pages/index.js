e868a7d4cead6877f9d6f569ccf78d7606fc8537
andirambana <andirambanaa@gmail.com>
2022-06-11 22:43:49 +0700

    add new to folder-structures

---
+++ frontend/src/Pages/index.js
@@ -0,0 +1,5 @@
+import { LandingPage } from "./LandingPage/landingPage";
+import { Login } from "./Login/login";
+import { Register } from "./Register/register";
+
+export { LandingPage, Login, Register };
